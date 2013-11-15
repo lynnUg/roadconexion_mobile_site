28c0a9f76fdd0e4a877c4479cf9ddb77948f2c4b
Lynn Asiimwe <lynnasiimwe@gmail.com>
2013-11-16 00:08:38 +0300

    test

--- mobile/models.py
+++ mobile/models.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import ( AbstractBaseUser, BaseUserManager, PermissionsMixin)
+ 
 from django.db import models
 # Create your models here.
 TYPE_CHOICES=(
@@ -8,10 +9,45 @@ TYPE_CHOICES=(
 ('Status','Status'),
 
 )
+class myUserManager(BaseUserManager):
+    def create_user(self, email,password, **extra_fields):
+        now = datetime.now()
+        user = self.model(email=email,is_active=True,is_staff=False,last_login=now,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email,password,**extra_fields):
+        u= self.create_user(email,password,**extra_fields)
+        u.is_staff= True
+        u.is_active= True
+        u.is_superuser=True
+        u.save(using=self._db)
+        return u
+class myUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(blank=True)
+    
+    USERNAME_FIELD = 'email'
+   
+    is_staff = models.BooleanField('staff status', default=False,
+        help_text='Designates whether the user can log into this admin '
+                    'site.')
+    is_active = models.BooleanField('active', default=True,
+        help_text='Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.')
+    def get_full_name(self):
+        full_name = '%s %s' % (self.email)
+        return full_name.strip()
+ 
+    def get_short_name(self):
+        return self.email
+ 
+    objects = myUserManager()
+
 class Report(models.Model):
     road_name = models.CharField(max_length=128, unique=True)
     report= models.TextField(blank=False)
     type_report=models.CharField(max_length=200,choices=TYPE_CHOICES,blank=False)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(myUser)
     def __unicode__(self):
         return self.road_name
--- roadtracker/urls.py
+++ roadtracker/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import (TemplateView,ListView)
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^$', TemplateView.as_view(template_name='front.html')),
+    url(r'', include('social_auth.urls')),
     # url(r'^$', 'roadtracker.views.home', name='home'),
     # url(r'^roadtracker/', include('roadtracker.foo.urls')),
 
@@ -13,5 +17,11 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+        urlpatterns += patterns(
+                'django.views.static',
+                (r'profile_images/(?P<path>.*)',
+                'serve',
+                {'document_root': settings.MEDIA_ROOT}), )
