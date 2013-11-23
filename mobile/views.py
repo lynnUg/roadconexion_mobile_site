fc61336749707645614bda0cc693dff49185fd0b
danogwok <danogwok@yahoo.co.uk>
2013-11-23 16:09:29 +0300

    url fixes

--- mobile/views.py
+++ mobile/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 
 #for the RESTful API
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
